083c0b5755772df35be83af97a7def2e9638e24b
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-11-16 04:54:28 +0100

    working on the service

---
+++ src/main/java/project/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,25 @@
+package project.departmentservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.departmentservice.entity.Department;
+import project.departmentservice.service.DepartmentService;
+
+@RestController
+@RequestMapping("/departments")
+@Slf4j
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/")
+    public Department saveDepartment(@RequestBody Department department){
+        log.info("Inside saveDepartment method of department controller.");
+        return departmentService.saveDepartment(department);
+    }
+}
